1892113995507be82b5e2936261294f5a50bd6f4
Tobias Lettos <lettos.tobias02@gmail.com>
2022-09-17 19:38:29 -0300

    cambios sobre el ngFor, ngClass, ngIf e INTERFACES

---
+++ celularesTandil/src/app/celulares-list/Celular.ts
@@ -0,0 +1,9 @@
+export interface Celular{
+    marca : string ,
+    tipo : string , 
+    nombre : string , 
+    precio : number, 
+    stock : number ,
+    imagen : string ,
+    clearance:boolean,
+}
--- celularesTandil/src/app/celulares-list/celulares-list.component.ts
+++ celularesTandil/src/app/celulares-list/celulares-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Celular } from './Celular';
 
 @Component({
   selector: 'app-celulares-list',
@@ -7,15 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CelularesListComponent implements OnInit {
 
-  celular = {
-    "marca" : "samsung",
-    "tipo" : "gama alta" , 
-    "nombre" : "samsung s21" , 
-    "precio" : "150.000" , 
-    "stock" : "disponible" ,
-    "imagen" : "./assets/img/samsungs21.jpg" ,
- 
-  }
+  celulares:  Celular [] = [
+    {
+        marca : "samsung",
+        tipo : "gama alta" , 
+        nombre : "samsung s21" , 
+        precio : 150.000 , 
+        stock : 13 ,
+        imagen : "./assets/img/samsungs21.jpg" ,
+        clearance : false,
+    } ,
+    {
+        marca : "iphone",
+        tipo : "gama alta" , 
+        nombre : "iphone 14 pro max" , 
+        precio : 500.000 , 
+        stock : 0 ,
+        imagen : "./assets/img/samsungs21.jpg" ,
+        clearance : true,
+    }
+  
+  ]
   constructor() { }
 
   ngOnInit(): void {
